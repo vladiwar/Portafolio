9d6f430306bf0eff79d906e0fde31588dae486e2
Vladimir Montiel <vladiwar1@gmail.com>
2022-07-29 17:28:17 -0500

    Animation and skills hover cards

---
+++ js/mypopper.js
@@ -0,0 +1,71 @@
+//Picture
+tippy("#picture", {
+  content:
+    "The key to immortality is primarily living a life worth remembering. By: Bruce Lee",
+});
+
+tippy("#nevadoToluca", {
+  content: "Nevado de Toluca, México",
+});
+
+//Skills
+tippy("#skillHtml", {
+  content: "HTML5",
+});
+
+tippy("#skillCss", {
+    content: "CSS3",
+  });
+
+
+  tippy("#skillJavascript", {
+    content: "Javascript",
+  });
+
+  tippy("#skillWordpress", {
+    content: "Wordpress",
+  });
+
+  tippy("#skillBootstrap", {
+    content: "Bootstrap",
+  });
+
+  tippy("#skillGit", {
+    content: "Git and Github",
+  });
+
+  tippy("#skillReact", {
+    content: "ReactJS",
+  });
+
+  tippy("#skillNode", {
+    content: "NodeJs",
+  });
+
+  tippy("#skillPhp", {
+    content: "PHP",
+  });
+
+  tippy("#skillPython", {
+    content: "Python",
+  });
+
+  tippy("#skillC", {
+    content: "C#",
+  });
+  tippy("#skillSql", {
+    content: "MYSQL",
+  });
+
+  tippy("#skillMongo", {
+    content: "MongoDB",
+  });
+
+  tippy("#skillFirebase", {
+    content: "Firebase",
+  });
+
+  tippy("#skillJava", {
+    content: "Java",
+  });
+
