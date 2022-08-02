4f13ff30abea9a1fdb4d01e0e0d7b4791770b4a5
Vladimir Montiel <vladiwar1@gmail.com>
2022-08-01 22:53:44 -0500

    Version 1.1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const picture = (element) =>{
     let samurai = "./../img/kojiro_Tanaka.jpg";
-    let vladi = "./../img/kojiro_Tanaka-jump.jpg";
+    let vladi = "./../img/kojiro_Tanaka_jump.jpg";
     element.src = element.bln ? samurai : vladi;
     element.bln = !element.bln;
 }
+
+AOS.init();
